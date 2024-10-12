bbcce806cb5c6e8cfd06a6f931fde964d81a9268
Abi-07 <Abhishekjoshi.2022@gmail.com>
2024-10-12 08:21:09 +0530

    Oyeamiko-next.js init

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
+import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'OyeAmiko',
+  description: 'Live Life Nomad Style',
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang='en'>
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <LoginModal />
+          <RentModal />
+          <SearchModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
